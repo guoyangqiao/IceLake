8cb2ed3b0dee38c104f8cb948a09d0ca5131b9e0
guoyangqiao <944734785@qq.com>
2019-12-28 15:13:10 +0800

    Some optimize

--- app/src/main/java/org/guoyangqiao/icelake/MainActivity.java
+++ app/src/main/java/org/guoyangqiao/icelake/MainActivity.java
@@ -1,18 +1,13 @@
 package org.guoyangqiao.icelake;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.widget.Button;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import org.guoyangqiao.icelake.callback.OnStartCallback;
@@ -20,55 +15,40 @@ import org.guoyangqiao.icelake.callback.OnStartCallback;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MAIN";
 
+    public static final String CHANNEL_ICELAKE = "org.guoyangqiao.icelake";
+
+    private LocationClient locationClient;
+    private Notification notification;
+    private boolean inFrontState = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String[] ACCESS_COARSE_LOCATION = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
-        for (String access : ACCESS_COARSE_LOCATION) {
-            if (ContextCompat.checkSelfPermission(this, access) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, ACCESS_COARSE_LOCATION, 400);
-            }
-        }
+        locationClient = initLocationClient();
+        createChannels();
+        notification = new Notification.Builder(getApplicationContext(), CHANNEL_ICELAKE)
+                .setContentTitle("IceLake")
+                .setContentText("My destiny")
+                .setSmallIcon(android.R.drawable.stat_notify_more)
+                .setAutoCancel(true).build();
+        notification.defaults = Notification.DEFAULT_SOUND; //设置为默认的声音
 
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
-
-        LocationClient locationClient = initLocationOption();
-        View viewById = findViewById(R.id.button2);
-        viewById.setOnClickListener((v -> {
-            Log.d(TAG, viewById.toString() + " start.....");
-            locationClient.start();
-            Log.d(TAG, viewById.toString() + " end.....");
-        }));
+        Button viewById = (Button) findViewById(R.id.button2);
+        viewById.setOnClickListener(event -> {
+                    if (inFrontState) {
+                        inFrontState = false;
+                        locationClient.disableLocInForeground(true);
+                        viewById.setText("Start");
+                        locationClient.stop();
+                    } else {
+                        locationClient.enableLocInForeground(1, notification);
+                        inFrontState = true;
+                        viewById.setText("Stop");
+                        locationClient.start();
+                    }
+                }
+        );
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
     /**
      * 初始化定位参数配置
@@ -76,11 +56,11 @@ public class MainActivity extends AppCompatActivity {
      * @return
      */
 
-    private LocationClient initLocationOption() {
+    private LocationClient initLocationClient() {
         LocationClient locationClient = new LocationClient(getApplicationContext());
         locationClient.registerLocationListener(new OnStartCallback());
         LocationClientOption locationOption = new LocationClientOption();
-        locationOption.setLocationMode(LocationClientOption.LocationMode.Battery_Saving);
+        locationOption.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
         locationOption.setCoorType("gcj02");
         locationOption.setIsNeedAddress(true);
         locationOption.setIsNeedLocationDescribe(true);
@@ -92,8 +72,21 @@ public class MainActivity extends AppCompatActivity {
         locationOption.SetIgnoreCacheException(false);
         locationOption.setOpenGps(true);
         locationOption.setIsNeedAltitude(false);
-        locationOption.setOpenAutoNotifyMode(3000, 10, LocationClientOption.LOC_SENSITIVITY_LOW);
+//        locationOption.setOpenAutoNotifyMode(3000, 10, LocationClientOption.LOC_SENSITIVITY_LOW);
+        locationOption.setOpenAutoNotifyMode();
         locationClient.setLocOption(locationOption);
         return locationClient;
     }
+
+
+    public void createChannels() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel androidChannel = new NotificationChannel(CHANNEL_ICELAKE, "ANDROID CHANNEL", NotificationManager.IMPORTANCE_DEFAULT);
+        androidChannel.enableLights(true);
+        androidChannel.enableVibration(true);
+        androidChannel.setLightColor(Color.GREEN);
+        androidChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+        notificationManager.createNotificationChannel(androidChannel);
+    }
+
 }
